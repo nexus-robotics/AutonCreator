254425a31b158bf4489a5dcc27fab84989da0752
Carson Brown <45776517+Zaymus@users.noreply.github.com>
2019-05-23 21:37:59 -0400

    Update autonCreator.java

--- src/ca/zaymus/autonCreator.java
+++ src/ca/zaymus/autonCreator.java
@@ -18,42 +18,10 @@ public class autonCreator extends JComponent
     
     private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     
-    private static void resize(String inputImagePath, String outputImagePath, int scaledWidth, int scaledHeight) throws IOException
-    {
-        // reads input image
-        File inputFile = new File(inputImagePath);
-        BufferedImage inputImage = ImageIO.read(inputFile);
-        
-        // creates output image
-        BufferedImage outputImage = new BufferedImage(scaledWidth,
-                scaledHeight, inputImage.getType());
-        
-        // scales the input image to the output image
-        Graphics2D g2d = outputImage.createGraphics();
-        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
-        g2d.dispose();
-        
-        // extracts extension of output file
-        String formatName = outputImagePath.substring(outputImagePath
-                .lastIndexOf(".") + 1);
-        
-        // writes to output file
-        ImageIO.write(outputImage, formatName, new File(outputImagePath));
-    }
-    
-    private static void resize(String inputImagePath, String outputImagePath, double percent) throws IOException
-    {
-        File inputFile = new File(inputImagePath);
-        BufferedImage inputImage = ImageIO.read(inputFile);
-        int scaledWidth = (int) (inputImage.getWidth() * percent);
-        int scaledHeight = (int) (inputImage.getHeight() * percent);
-        resize(inputImagePath, outputImagePath, scaledWidth, scaledHeight);
-    }
     
     public static void main(String[] args) throws IOException
     {
         String imagePath = "images/TowerTakeoverField.png";
-        String outputImagePath = "images/TowerTakeoverField_Resized.png";
         
         controls = new JPanel();
         controls.setLayout(null);
@@ -72,16 +40,18 @@ public class autonCreator extends JComponent
         
         field.setBounds(controls.getWidth(), 0, controls.getWidth(), window.getHeight());
         
-        Image image = ImageIO.read(new File(imagePath));
+        Image image = ImageIO.read(imagePath);
         float scalePercent = (float)field.getHeight() / (float)image.getHeight(null);
+        
         System.out.println(field.getHeight() + " , " + image.getHeight(null) + " , " + scalePercent);
-        resize(imagePath, outputImagePath, scalePercent);
-        Image image_resized = ImageIO.read(new File(outputImagePath));
-        JLabel picLabel = new JLabel(new ImageIcon(image_resized));
+        
+        Image image_resized = image.getScaledInstance(image.getWidth * scalePercent, 
+                                                        image.getHeight * scalePercent, Image.SCALE_SMOOTH);
+        
+                                                        JLabel picLabel = new JLabel(new ImageIcon(image_resized));
         picLabel.setBounds(0,0, field.getWidth(), field.getHeight());
+        
         field.add(picLabel);
-        
-        
         field.repaint();
         window.setVisible(true);
     }//end of main
